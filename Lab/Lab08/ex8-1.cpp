137496f90e2a8944daff95a5f445566c17f5ea31
Jerry <jerry871002@gmail.com>
2018-04-23 23:48:32 +0800

    Add lab 8

---
+++ Lab/Lab08/ex8-1.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+class Point2D
+{
+private:
+	int x;
+	int y;
+public:
+	// add any member if necessary
+	void setCor(int a, int b) {
+		x = a;
+		y = b;
+	}
+	int getX() {return x;}
+	int getY() {return y;}
+};
+
+template<class T>
+T *new1D(int n, int k) {
+	T *vec = new T[n];
+	for (int i = 0; i < n; i++)
+		vec[i] = k;
+	return vec;
+}
+
+template<>
+Point2D *new1D(int n, int k) {
+	Point2D *vec = new Point2D[n];
+	for(int i = 0; i < n; i++)
+		vec[i].setCor(k, k);
+	return vec;
+}
+
+template<class T>
+void rand1D(T *vec, int n) {
+	for(int i = 0; i < n; i++)
+		vec[i] = rand() % 10 + 1;
+}
+
+template<>
+void rand1D(double *vec, int n) {
+	for(int i = 0; i < n; i++)
+		vec[i] = (rand() % 1001) * 0.01;
+}
+
+template<>
+void rand1D(char *vec, int n) {
+	for(int i = 0; i < n; i++)
+		vec[i] = (char)((rand() % 26) + 97);
+}
+
+template<>
+void rand1D(Point2D *vec, int n) {
+	for(int i = 0; i < n; i++)
+		vec[i].setCor(rand() % 10, rand() % 10);
+}
+
+template<class T>
+void display1D(T *vec, int n) {
+	for(int i = 0; i < n; i++)
+		cout << vec[i] << " ";
+	cout << endl;
+}
+
+template<>
+void display1D(Point2D *vec, int n) {
+	for(int i = 0; i < n; i++)
+		cout << "(" << vec[i].getX() << " , " << vec[i].getY() << ") ";
+	cout << endl;
+}
+
+template<class T>
+void sort1D(T *vec, int n) {
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n-i-1; j++) {
+			if(vec[j] > vec[j+1]) {
+				T temp = vec[j];
+				vec[j] = vec[j+1];
+				vec[j+1] = temp;
+			}
+		}
+	}
+}
+
+template<>
+void sort1D(Point2D *vec, int n) {
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n-i-1; j++) {
+			if(vec[j].getX() > vec[j+1].getX()) {
+				Point2D temp = vec[j];
+				vec[j] = vec[j+1];
+				vec[j+1] = temp;
+			} else if(vec[j].getX() == vec[j+1].getX()) {
+				if(vec[j].getY() > vec[j+1].getY()) {
+					Point2D temp = vec[j];
+					vec[j] = vec[j+1];
+					vec[j+1] = temp;
+				}
+			}
+		}
+	}
+}
+
+template<class T>
+void analysis(int n, int k = 0) {
+	T *vec = new1D<T> (n, k);
+	rand1D<T>(vec,n);
+	// for int 1~10, for double 0.00~10.00, for char a~z,
+	// for Point2D x: 0~9 y:0~9
+	display1D<T>(vec,n);
+	sort1D<T>(vec,n);
+	display1D<T>(vec,n);
+}
+
+int main()
+{
+	int n;
+	cout << "Enter n: ";
+	cin >> n;
+	srand(1);
+	analysis<int>(n);
+	analysis<double>(n);
+	analysis<char>(n);
+	analysis<Point2D>(n);
+	return 0;
+}
---
+++ Lab/Lab08/vector.h
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Vector;
+class Point2D;
+
+template <class T>
+class Vector {
+private:
+	int len;
+	T* vec;
+public:
+	// add any member if necessary
+
+	Vector(int n, double x) {
+		len = n;
+		vec = new T[n];
+		for(int i = 0; i < len; i++)
+			vec[i] = x;
+	}
+
+	Vector(int n, T *x) {
+		len = n;
+		vec = x;
+	}
+
+	void display() {
+		for(int i = 0; i < len; i++)
+			cout << vec[i] << " ";
+		cout << endl;
+	}
+
+	template<class S>
+	Vector<S> & operator+=(const Vector<S> &addend) {
+		for(int i = 0; i < len; i++)
+			vec[i] += addend.vec[i];
+		return (*this);
+	}
+
+	template<class S>
+	friend S dot(const Vector<S> &, const Vector<S> &);
+};
+
+template<class T>
+T dot(const Vector<T> &a, const Vector<T> &b) {
+	T result;
+	result = 0;
+	for(int i = 0; i < a.len; i++)
+		result += (a.vec[i] * b.vec[i]);
+	return result;
+}
+
+class Point2D
+{
+private:
+	int x;
+	int y;
+public:
+	void setCor(int a, int b) {
+		x = a;
+		y = b;
+	}
+	int getX() const {return x;}
+	int getY() const {return y;}
+	Point2D& operator+=(const Point2D& aDot) {
+		x += aDot.x;
+		y += aDot.y;
+		return (*this);
+	}
+	Point2D operator*(const Point2D& aDot) {
+		Point2D result;
+		result.setCor(x * aDot.x, y * aDot.y);
+		return result;
+	}
+	void operator=(double num) {
+		x = (int)num;
+		y = (int)num;
+	}
+	friend ostream& operator<<(ostream&, const Point2D&);
+};
+
+template<class T>
+void rand1D(T *vec, int n) {
+	for(int i = 0; i < n; i++)
+		vec[i].setCor(rand() % 10, rand() % 10);
+}
+
+ostream& operator<<(ostream& out, const Point2D& dot) {
+	out << "(" << dot.getX() << " , " << dot.getY() << ")";
+	return out;
+}
