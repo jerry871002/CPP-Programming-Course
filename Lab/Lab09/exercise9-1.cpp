927aabf59fcfc39b9a022d5349aa61aebced557b
Jerry <jerry871002@gmail.com>
2018-04-30 19:42:53 +0800

    Lab09 finished

--- Lab/Lab09/exercise9-1.cpp
+++ Lab/Lab09/exercise9-1.cpp
@@ -1,18 +1,66 @@
-#include <iostream> #include <fstream> #include <vector> #include <algorithm> using namespace std;
-class Complex{ ...
+#include <iostream> 
+#include <fstream> 
+#include <vector> 
+#include <algorithm> 
+
+using namespace std;
+
+class Complex{ 
+public:
+	Complex(float numX = 0, float numY = 0) : x(numX), y(numY) {
+		//empty
+	}
+
+	float x, y;
 };
-bool operator < (const Complex& a,const Complex& b) {
-... }
-void ReadFile(ifstream& infile, vector<Complex>& vec) { ...
+
+bool operator < (const Complex& a, const Complex& b) {
+	if(a.x > b.x)
+		return true;
+	else if(a.x == b.x && a.y > b.y)
+		return true;
+	else
+		return false;
 }
+
+void ReadFile(ifstream& infile, vector<Complex>& vec) { 
+	for(int i = 0; i < 5; i++) {
+		float numX, numY;
+		infile >> numX >> numY;
+		cout << "#" << numX << " #" << numY << endl;
+		Complex temp(numX, numY);
+		vec.push_back(temp);
+	}	
+}
+
+ostream& operator << (ostream& out, const Complex& a) {
+	out << "(" << a.x << "," << a.y << ")";
+	return out;
+}
+
 int main(int argc, char** argv) {
-	if(argc != 2) return 0; ifstream infile(argv[1]); if(!infile.is_open()) {
-cout << "can not oepn file " << endl;
-return 0; }
-vector<Complex> vec; ReadFile(infile, vec);
-cout << "Original sequence:" << endl; for(size_t i = 0; i< vec.size(); i++)
-cout << vec[i] <<" "; cout << endl;
-sort(vec.begin(),vec.end());
-cout << "Sorted sequence:" << endl; for(size_t i = 0; i< vec.size(); i++)
-cout << vec[i] <<" "; cout << endl;
-return 0; }
+	if(argc != 2) 
+		return 0;
+
+	ifstream infile(argv[1]); 
+	if(!infile.is_open()) {
+		cout << "can not oepn file " << endl;
+		return 0; 
+	}
+
+	vector<Complex> vec; 
+	ReadFile(infile, vec);
+
+	cout << "Original sequence:" << endl; 
+	for(size_t i = 0; i< vec.size(); i++)
+		cout << vec[i] << " "; 
+	cout << endl;
+	sort(vec.begin(),vec.end());
+
+	cout << "Sorted sequence:" << endl; 
+	for(size_t i = 0; i< vec.size(); i++)
+		cout << vec[i] << " "; 
+	cout << endl;
+
+	return 0; 
+}
--- Lab/Lab09/exercise9-2.cpp
+++ Lab/Lab09/exercise9-2.cpp
@@ -1,26 +1,84 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
 class Book {
-string Name; // name of book
-int Number; // how many books in your store
+	string Name; // name of book
+	int Number; // how many books in your store
 public:
-Book(){;}
-Book(string _name, int _number):Name(_name),Number(_number){} ...
+	Book() {
+		//empty
+	}
+	Book(string _name, int _number) : Name(_name), Number(_number) {
+		//empty
+	} 
+	void sellBook(int num) {
+		if(Number > num) {
+			Number -= num;
+			cout << "Sucessfully sold." << endl;
+		} else
+			cout << "Not enough stock";
+	}
+
+	friend ostream& operator << (ostream&, Book);
 };
-int main() {
-map<int, Book> BookMap;
-BookMap[1] = Book("C++ Programing", 1); BookMap[2] = Book("Java", 2); BookMap[3] = Book("Python", 3);
-int step;
-while(1) {
-cout <<"1. show information of book store" << endl; cout <<"2. add new books" << endl;
-cout <<"3. sell books " << endl; cout <<"4. exit" << endl;
-cin >> step;
-if(step==1) {
-... }
-else if(step==2) {
-cout <<"enter Name and Number of new book" << endl; ...
+
+ostream& operator << (ostream& out, Book someBook) {
+	out << setw(25) << someBook.Name << setw(10) << someBook.Number;
+	return out;
 }
-else if(step==3) {
-cout << "enter sell id" << endl;
-... }
-else if(step==4) { break;
-} }
-return 0; }
+
+int main() {
+	map<int, Book> BookMap;
+	map<int, Book>::iterator iter;
+	BookMap[1] = Book("C++ Programing", 1); 
+	BookMap[2] = Book("Java", 2); 
+	BookMap[3] = Book("Python", 3);
+
+	int step;
+	while(1) {
+		cout <<"1. show information of book store" << endl; 
+		cout <<"2. add new books" << endl;
+		cout <<"3. sell books " << endl; 
+		cout <<"4. exit" << endl;
+		cin >> step;
+	
+		if(step==1) {
+			cout << "ID" << setw(25) << "Name" << setw(10) << "Number" << endl;
+			for(iter = BookMap.begin(); iter != BookMap.end(); iter++) {
+				cout << iter->first ;
+				cout << setw(10) << iter->second << endl;
+			}
+		}
+		else if(step==2) {
+			string bookName; 
+			int numOfBook;
+			cout << "enter Name and Number of new book" << endl;
+			getline(cin, bookName);
+			getline(cin, numOfBook);
+			//cin >> bookName >> numOfBook;
+			BookMap[BookMap.size()+1] = Book(bookName, numOfBook);
+		}
+		else if(step==3) {
+			int sellId, sellNum;
+			cout << "enter sell id" << endl;
+			cin >> sellId;
+			cout << "enter sell number" << endl;
+			cin >> sellNum;
+
+			iter = BookMap.find(sellId);
+			if(iter != BookMap.end())
+				iter->second.sellBook(sellNum);
+			else
+				cout << "No such book" << endl;
+		}
+		else if(step==4) { 
+			break;
+		} 	
+	}
+
+	return 0; 
+}
